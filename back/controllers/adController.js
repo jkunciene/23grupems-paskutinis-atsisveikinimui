8c5b2fbef1f02bace41c3203a6c5ac73b2d755bd
jkunciene <j.kunciene@gmail.com>
2023-06-27 14:08:52 +0300

    set new ad

--- back/controllers/adController.js
+++ back/controllers/adController.js
@@ -5,12 +5,43 @@ const asyncHandler = require('express-async-handler');
 // @route GET /api/ad
 // @access PUBLIC
 
-const getAllAds = asyncHandler( async (req, res)=>{
+const getAllAds = asyncHandler(async (req, res) => {
     const ads = await Ad.find();
     res.status(200).send(ads);
 })
 
+
+// @desc Set new Ad
+// @route POST /api/ad
+// @access PRIVAT (simple user)
+
+const setAd = asyncHandler(async (req, res) => {
+    const { name, description, image, price, category } = req.body
+    if (!name || !description || !image || !price || !category) {
+      res.status(400)
+      throw new Error('Please add all fields')
+    }
+
+    const ad = await Ad.create({
+      name,
+      description,
+      image,
+      price,
+      category,
+      user: req.user.id
+    })
+    if (ad) {
+      res.status(201).send(ad)
+    } else {
+      res.status(400)
+      throw new Error('Invalid data')
+    }
+  })
+
+
+//get user's ads
+
 module.exports = {
     getAllAds,
-    
+    setAd
 }
--- back/routes/adRoutes.js
+++ back/routes/adRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { protect } = require('../middleware/authMiddleware');
+
 const {
     getAllAds,
+    setAd
 } = require('../controllers/adController');
 
-router.get('/', getAllAds);
+router.get('/', getAllAds).post('/', protect, setAd)
 
 module.exports = router
